03f69ef6349a57c751d8ebb98c8bbd8d4b80e738
Bae Tae Young <sk8bty@gmail.com>
2021-07-19 22:41:05 +0900

    main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace screenMagic {
+    /**
+     * Address LEDs linealy row first
+     */
+    //% blockID=screenmagicplotat
+    //% block="plot LED at index"
+    //% index.min=0 index.max=25
     export function plotAt(index: number) : void {
         index |= 0
         const y = Math.floor(index / 5);
